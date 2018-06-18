3587ab25a09d0cea1fda85c104f000a8ca5c8ac5
Sharon Miller <sharon@sharonmillercreative.com>
2018-06-18 12:40:34 -0700

    created JS Object Literals for each store location

---
+++ js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// 1 Stores the min/max hourly customers, and the average cookies per customer, in object properties
+
+// 2 Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
+
+// 3 Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+
+//4  Store the results for each location in a separate array... perhaps as a property of the object representing that location
+
+// 5 Display the values of each array as unordered lists in the browser
+var collegeAndPence = {
+  minCust: 23,
+  maxCust: 65, 
+  avgCookieSale: 6.3,
+};
+
+var chandlerAnd14th = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSale: 1.2,
+};
+
+var fresnoAnd14th = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSale: 3.7,
+};
+
+var minnesotaAndBond = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookieSale: 2.3,
+};
+
+var neNeffAndeNeWilliamson = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookieSale: 4.6,
+};
+
