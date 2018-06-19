9215835045823d2b7e9310194dddbe5fd17ec606
Sharon Miller <sharon@sharonmillercreative.com>
2018-06-19 14:05:02 -0700

    finished first store, need to do rest of stores and HTML

--- js/app.js
+++ js/app.js
@@ -1,63 +1,101 @@
 'use strict';
+//create global variables
+var storeContainer = document.getElementById('store-container');
+var hourArray = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-// 1 Stores the min/max hourly customers, and the average cookies per customer, in object properties
-
-// 2 Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-// Math.floor(Math.random() * 50 + 30),
-
-// 3 Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-
-//4  Store the results for each location in a separate array... perhaps as a property of the object representing that location
-
-// 5 Display the values of each array as unordered lists in the browser
-//names of stores collegeAndPence, chandlerAnd14th, fresnoAnd14th, minnesotaAndBond, neNeffAndNeWilliamson
-var stores = [];
-var liEl; 
-var storeListUlEl = document.getElementById('store-list');
-console.log(storeListUlEl.textContent);
-var hours;
-
+//College and Pence
 var collegeAndPence = {
   name: 'College And Pence',
   minCust: 23,
   maxCust: 65,
   avgCookiePerSale: 6.3,
-  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-  randomCust: function () {
-    //TODO: fix math
-    return Math.floor(Math.random() * this.minCust + this.maxCust);
-  },
-  cookiesHour: function () {
-    return this.avgCookiePerSale * this.randomCust();
-  },
-  cookiesPerHour: []
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookies: 0,
 };
-console.log(collegeAndPence.randomCust());
-console.log(collegeAndPence.cookiesHour());
-console.log(collegeAndPence);
-
-
-//calculate the hours with a for loop
-for ( hours = 0; hours < 15; hours++) {
-  collegeAndPence.cookiesPerHour.push(collegeAndPence.cookiesHour());
-}
-
-collegeAndPence.render = function () {
-  var liEl = document.createElement('li');
-  liEl.textContent = this.cookiesPerHour;
-  storeListUlEl.appendChild();
+//create random number of customers using a method
+collegeAndPence.randomCust = function () {
+  return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.maxCust);
+};
+//create loop for customers per hour
+collegeAndPence.calculateCustomersPerHour = function () {
+  for(var i in hourArray){
+    this.customersPerHour[i] = (this.randomCust());
+    // console.log(this.customersPerHour[i]);
+  }
+};
+//function to calculate total cookies per hour - create variables and then a loop to move through the array.
+collegeAndPence.calculateCookiesPerHour = function () {
+  this.calculateCustomersPerHour();
+  for (var i in hourArray) {
+    this.cookiesPerHour[i] = Math.ceil(this.customersPerHour[i] * this.avgCookiePerSale);
+    this.totalCookies += this.cookiesPerHour[i];
+  }
+  // console.log(this.cookiesPerHour);
+  // console.log(this.totalCookies);
 };
 
-liEl = document.createElement('li');
 
-for (var i in this.cookiesPerHour) {
-  liEl = document.createElement('li');
 
-  liEl.textContent = this.cookiesPerHour[i].hours;
-  ulEl.appendChild(liEl);
-}
+//write cookies per hour to HTML
+collegeAndPence.showHtmlCookiesPerHour = function(){
+  this.calculateCookiesPerHour();
+  //create header
+  var h1El = document.createElement('h1');
+  h1El.textContent = this.name;
+  storeContainer.appendChild(h1El);
+  //creat unordered list
+  var ulEl = document.createElement('ul');
+  for (var i in hourArray) {
+    var liEl = document.createElement('li');
+    liEl.textContent = hourArray[i] + (': ') + this.cookiesPerHour[i] + ' cookies';
+    ulEl.appendChild(liEl);
+    storeContainer.appendChild(ulEl);
+    console.log(liEl.textContent);
+    console.log(this.cookiesPerHour[i]);
+  }
+  liEl.textContent = 'Total: ' + this.totalCookies + ' cookies';
+};
 
-liEl.textContent = this.cookiesPerHour;
+
+collegeAndPence.showHtmlCookiesPerHour();
+
+
+
+
+
+
+
+
+// cookiesHour = function  () {
+//   return this.avgCookiePerSale * this.randomCust();
+// };
+
+// //calculate the hours with a for loop
+// for ( hours = 0; hours < 15; hours++) {
+//   collegeAndPence.cookiesPerHour.push(collegeAndPence.cookiesHour());
+
+// collegeAndPence.render = function () {
+//   var liEl = document.createElement('li');
+//   liEl.textContent = this.cookiesPerHour;
+//   storeListUlEl.appendChild();
+//   return toString.this.cookiesPerHour;
+// }
+
+
+
+// this code does not work:
+// liEl = document.createElement('li');
+
+// for (var i in this.cookiesPerHour) {
+//   liEl = document.createElement('li');
+
+//   liEl.textContent = collegeAndPence.cookiesPerHour[i].hours;
+//   liEl.appendChild(liEl);
+//   console.log(liEl.textContent);
+// }
+
+// liEl.textContent = collegeAndPence.cookiesPerHour;
 
 // console.log(collegeAndPence.cookiesPerHour);
 // var chandlerAnd14th = {
