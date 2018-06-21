f44931617b187543ecfc7efd6ddacf3bd256d383
Sharon Miller <sharon@sharonmillercreative.com>
2018-06-21 08:39:59 -0700

    corrected cookieperhour

--- js/app.js
+++ js/app.js
@@ -19,20 +19,20 @@ function Store(name, minCust, maxCust, avgCookiePerSale) {
 }
 // console.log(storeArray);
 //create Store Instances
-new Store ('College and Pence', 23, 65, 6.3);
-new Store ('Chandler and 14th', 3, 24, 1.2);
-new Store ('Fresno and 14th', 11, 38, 3.7);
-new Store ('Minnesota and Bond', 20, 38, 2.3);
-new Store ('NE Neff and NE Williamson', 2, 16, 4.6);
+new Store('College and Pence', 23, 65, 6.3);
+new Store('Chandler and 14th', 3, 24, 1.2);
+new Store('Fresno and 14th', 11, 38, 3.7);
+new Store('Minnesota and Bond', 20, 38, 2.3);
+new Store('NE Neff and NE Williamson', 2, 16, 4.6);
 
 Store.prototype.randomCust = function () {
-  for(var i in hourArray){
+  for (var i in hourArray) {
     var randomCust = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     this.customersPerHour[i] = randomCust;
   }
 };
 
-Store.prototype.calculateCookiesPerHour = function (){
+Store.prototype.calculateCookiesPerHour = function () {
   this.randomCust();
   this.totalCookies = 0;
 
@@ -43,13 +43,13 @@ Store.prototype.calculateCookiesPerHour = function (){
 
 };
 //create header row with hoursArray - need loop
-var makeHeaderRow = function() {
+var makeHeaderRow = function () {
   var trEl = document.createElement('tr');
   var thEl = document.createElement('th');
   thEl.textContent = 'Store Locations';
   trEl.appendChild(thEl);
   console.log(trEl);
-  for(var i in hourArray) {
+  for (var i in hourArray) {
     // trEl = document.createElement('tr');
     thEl = document.createElement('th');
     thEl.textContent = hourArray[i];
@@ -59,7 +59,7 @@ var makeHeaderRow = function() {
 };
 makeHeaderRow();
 //create table
-Store.prototype.render = function(){
+Store.prototype.render = function () {
   this.calculateCookiesPerHour();
   //create tr
   var trEl = document.createElement('tr');
@@ -70,7 +70,7 @@ Store.prototype.render = function(){
   //append the TD to the TR
   trEl.appendChild(tdEl);
   //create for loop with array cookies per hour
-  for(var i in this.cookiesPerHour){
+  for (var i in this.cookiesPerHour) {
     tdEl = document.createElement('td');
     tdEl.textContent = this.cookiesPerHour[i];
     trEl.appendChild(tdEl);
@@ -78,25 +78,25 @@ Store.prototype.render = function(){
   storeTable.appendChild(trEl);
   //render store array
 };
-for(var i in storeArray) {
+for (var i in storeArray) {
   storeArray[i].render();
 }
-//footer total row
-function makeFooter() {
-var tdEl = document.createElement('td');
-trEl.textContent= 'Total:';
-storeTable.appendChild(trEl);
-var completeTotal = 0;
-for(var i in hourArray){
-  var totalHourly = 0;
-  for (var j in storeArray){
-    
-  }
+// //footer total row
+// function makeFooter() {
+// var tdEl = document.createElement('td');
+// trEl.textContent= 'Total:';
+// storeTable.appendChild(trEl);
+// var completeTotal = 0;
+// for(var i in hourArray){
+//   var totalHourly = 0;
+//   for (var j in storeArray){
 
-}
+//   }
 
-}
-makeFooter();
+// }
+
+// }
+// makeFooter();
 
 // tdEl = document.createElement('td');
 // tdEl.textContent = this.totalCookies;
@@ -117,8 +117,8 @@ makeFooter();
 //event handler function
 function handlerFormSubmit(event) {
   event.preventDefault();
-  if (!event.target.name.value || !event.target.minCust.value || !event.target.maxCust.value || !event.target.avgCookiesPerSale){
-    return alert ('Fields may not be empty');
+  if (!event.target.name.value || !event.target.minCust.value || !event.target.maxCust.value || !event.target.avgCookiePerSale) {
+    return alert('Fields may not be empty');
   }
 }
 
@@ -137,13 +137,13 @@ function handlerFormSubmit(event) {
 
 
 
-  // console.log('log of the event object', event);
-  // console.log('log of the event.target', event.target);
-  // console.log('log of the event.target.name', event.target.name.value);
-  // console.log(event.target.minCust.value);
+// console.log('log of the event object', event);
+// console.log('log of the event.target', event.target);
+// console.log('log of the event.target.name', event.target.name.value);
+// console.log(event.target.minCust.value);
 
-  
-   
+
+
 var storeForm = document.getElementById('add-location');
 
 // var storeForm = document.getElementbyId ('store-form');
